a080e6b1dfdd104b2ba6f58cac1abf95b63d4867
vanshanium <vansh.singh8272@gmail.com>
2026-05-19 23:16:55 +0530

    Title : Router/Structure Add : Basic Files Fix: None Doc : None

--- apps/src/app/(tabs)/index.tsx
+++ apps/src/app/(tabs)/index.tsx
@@ -1,33 +1,9 @@
-import { View, Text, StyleSheet } from "react-native";
+import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello React Native 👋</Text>
-      <Text style={styles.subtitle}>Your app is working successfully.</Text>
+    <View>
+      <Text>Index</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#111827",
-    alignItems: "center",
-    justifyContent: "center",
-    padding: 20,
-  },
-
-  title: {
-    fontSize: 28,
-    fontWeight: "bold",
-    color: "#ffffff",
-    marginBottom: 10,
-  },
-
-  subtitle: {
-    fontSize: 16,
-    color: "#9ca3af",
-    textAlign: "center",
-  },
-});
