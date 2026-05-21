9ca0cd77c287ba7228570bc7e3b25213da61fe02
vanshanium <vansh.singh8272@gmail.com>
2026-05-21 16:21:24 +0530

    Title : Working on the background Add : Modular Code Fix : None Doc : None

--- apps/src/app/(tabs)/_layout.tsx
+++ apps/src/app/(tabs)/_layout.tsx
@@ -2,8 +2,13 @@ import { Tabs } from "expo-router";
 
 export default function TabsLayout() {
   return (
-    <Tabs>
-      <Tabs.Screen name="index" options={{ title: "Pahad" }} />
+    <Tabs
+      // This line Romoves the Top Bar
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Tabs.Screen name="index" options={{ title: "Home" }} />
       <Tabs.Screen name="goals" options={{ title: "Goals" }} />
       <Tabs.Screen name="domains" options={{ title: "Domains" }} />
     </Tabs>
