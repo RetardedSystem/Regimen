a883ca5b404e17edc36865dde7817adf0af75e53
Vansh Singh <vansh.singh8272@gmail.com>
2026-05-26 23:43:56 +0530

    Vansh (#9)
    * Title : Tabbar is nearly done, Icons are remaining
    Add : TabBar Component
    Fix : Colors and Fonts
    Doc : None
    
    * Title : Setting up Database
    Added : Database file
    Fix : Index.tsx
    Doc : Need to work on
    
    * Icons on the NavBar & Initialized the Database
    Added : Icons & db file
    Fix : None
    Doc : None
    
    ---------
    
    Co-authored-by: Priya Chelani <76565608+Priya1202@users.noreply.github.com>

--- apps/src/app/(tabs)/_layout.tsx
+++ apps/src/app/(tabs)/_layout.tsx
@@ -1,28 +1,66 @@
 import { Tabs } from "expo-router";
 import TabBar from "../../components/TabBar";
 import Colors from "../../constants/Colors";
+import Icons from "../../constants/Icons";
+
+const icon_size = 20;
 
 export default function TabsLayout() {
   return (
     <Tabs
       tabBar={(props) => <TabBar {...props} />}
-      screenOptions={{
-        headerShown: false,
-        tabBarActiveTintColor: Colors.icon_active,
-        tabBarInactiveTintColor: Colors.white,
-        tabBarStyle: {
-          position: "absolute",
-          backgroundColor: "transparent",
-          height: "100%",
-          borderTopWidth: 0,
-          elevation: 0,
-        },
-        tabBarHideOnKeyboard: true,
-      }}
+      screenOptions={tabBarOptions}
     >
-      <Tabs.Screen name="index" options={{ title: "Home" }} />
-      <Tabs.Screen name="goals" options={{ title: "Goals" }} />
-      <Tabs.Screen name="domains" options={{ title: "Domains" }} />
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ focused }) =>
+            focused ? (
+              <Icons.homeActive width={icon_size} height={icon_size} />
+            ) : (
+              <Icons.home width={icon_size} height={icon_size} />
+            ),
+        }}
+      />
+      <Tabs.Screen
+        name="goals"
+        options={{
+          title: "Goals",
+          tabBarIcon: ({ focused }) =>
+            focused ? (
+              <Icons.goalsActive width={icon_size} height={icon_size} />
+            ) : (
+              <Icons.goals width={icon_size} height={icon_size} />
+            ),
+        }}
+      />
+      <Tabs.Screen
+        name="domains"
+        options={{
+          title: "Domains",
+          tabBarIcon: ({ focused }) =>
+            focused ? (
+              <Icons.domainsActive width={icon_size} height={icon_size} />
+            ) : (
+              <Icons.domains width={icon_size} height={icon_size} />
+            ),
+        }}
+      />
     </Tabs>
   );
 }
+
+const tabBarOptions = {
+  headerShown: false,
+  tabBarActiveTintColor: Colors.icon_active,
+  tabBarInactiveTintColor: Colors.white,
+  tabBarStyle: {
+    position: "absolute",
+    backgroundColor: "transparent",
+    height: "100%",
+    borderTopWidth: 0,
+    elevation: 0,
+  },
+  tabBarHideOnKeyboard: true,
+};
