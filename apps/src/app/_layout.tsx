754a317d248b505c3c2be455f7a00af826e4e5bb
vanshanium <vansh.singh8272@gmail.com>
2026-05-29 14:05:32 +0530

    Title : Initialized Database & Connected with KanbanColumn Add : Database Files Fix : Priya's Code Docs : KanbanColumn File

--- apps/src/app/_layout.tsx
+++ apps/src/app/_layout.tsx
@@ -1,5 +1,16 @@
 import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { initDB } from "@/databases/initDB";
+import { useEffect } from "react";
 
 export default function RootLayout() {
+  const [loaded] = useFonts({
+    Manjari_Regular: require("../constants/Fonts/Manjari-Regular.ttf"),
+    Manjari_Bold: require("../constants/Fonts/Manjari-Bold.ttf"),
+    Manjari_Thin: require("../constants/Fonts//Manjari-Thin.ttf"),
+  });
+  useEffect(() => {
+    initDB();
+  }, []);
   return <Stack screenOptions={{ headerShown: false }} />;
 }
--- apps/src/components/KanbanColumn.tsx
+++ apps/src/components/KanbanColumn.tsx
@@ -1,26 +1,152 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, ViewStyle, StyleSheet } from "react-native";
+import Colors from "../constants/Colors";
+import { getTasksByStatus } from "@/databases/getTasksByStatus";
+import { useEffect, useState } from "react";
 
 type Prop = {
-  title: String;
+  title: "todo" | "in_progress" | "done";
 };
 
+/**
+ * KanbanColumn is a component that represents a column in a Kanban board. It takes a title prop that renders the appropriate styles and content based on the title.
+ *
+ * @param {Prop} props - The Prop object containing the title of the Kanban column, which can be "todo", "in_progress", or "done".
+ * @returns {JSX.Element} A React element representing the Kanban column with its header and content.
+ */
+
 export default function KanbanColumn(props: Prop) {
+  let container: ViewStyle = {};
+  let header: ViewStyle = {};
+  const [tasks, setTasks] = useState<any[]>([]);
+
+  useEffect(() => {
+    async function load() {
+      const data = await getTasksByStatus(props.title);
+
+      setTasks(data);
+    }
+
+    load();
+  }, [props.title]);
+
+  console.log("This is the task", tasks);
+
+  // Determine the styles and title based on the provided title prop
+  let title = "";
+  if (props.title === "todo") {
+    container = styles.todo_container;
+    title = "Todo";
+    header = styles.todo_header;
+  } else if (props.title === "in_progress") {
+    container = styles.in_progress_container;
+    title = "Progress";
+    header = styles.in_progress_header;
+  } else if (props.title === "done") {
+    container = styles.done_container;
+    title = "Done";
+    header = styles.done_header;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>{props.title}</Text>
+    <View style={container}>
+      <View style={header}>
+        <View style={styles.task_count}>
+          <Text style={{ color: Colors.black, fontSize: 12 }}>
+            {tasks.length}
+          </Text>
+        </View>
+        <Text style={styles.title}>{title}</Text>
+      </View>
+      {/* Header */}
+
+      {props.title === "todo" && (
+        <View>
+          <Text>Todo Section</Text>
+        </View>
+      )}
     </View>
   );
 }
 
+// Corner Radius Cunt
+const borderRadius = 10;
+
+// Global Kanban Element Style, all Kanban Column inherits it.
+const container: ViewStyle = {
+  position: "absolute",
+  height: "30%",
+  width: "44%",
+  borderRadius: borderRadius,
+  backgroundColor: Colors.white,
+  alignItems: "center",
+  shadowOffset: {
+    width: 2,
+    height: 4,
+  },
+  shadowOpacity: 0.2,
+  shadowRadius: 10,
+  elevation: 2,
+};
+
+// Global Kanban Column Header Style, all Kanban Column Header inherits it.
+const header: ViewStyle = {
+  height: 40,
+  width: "100%",
+  borderTopLeftRadius: borderRadius,
+  borderTopRightRadius: borderRadius,
+  alignItems: "center",
+  justifyContent: "center",
+};
+
+// Styles for each Kanban Column, they inherit the global styles and add specific properties
 const styles = StyleSheet.create({
-  container: {
+  todo_container: {
+    ...container,
+    height: "50%",
+    left: "4%",
+    bottom: "4%",
+    shadowColor: Colors.red,
+  },
+  in_progress_container: {
+    ...container,
+    height: "25%",
+    right: "4%",
+    bottom: "29%",
+    shadowColor: Colors.yellow,
+  },
+  done_container: {
+    ...container,
+    height: "24%",
+    right: "4%",
+    bottom: "4%",
+    shadowColor: Colors.blue,
+  },
+  todo_header: {
+    ...header,
+    backgroundColor: Colors.red,
+  },
+  in_progress_header: {
+    ...header,
+    backgroundColor: Colors.yellow,
+  },
+  done_header: {
+    ...header,
+    backgroundColor: Colors.blue,
+  },
+  title: {
+    fontFamily: "Outfit-Bold",
+    fontSize: 20,
+    color: Colors.white,
+    textTransform: "uppercase",
+  },
+  task_count: {
     position: "absolute",
-    height: "40%",
-    width: "40%",
-    left: "6%",
-    bottom: "5%",
-    borderRadius: 10,
-    backgroundColor: "black",
+    left: 10,
+    width: 23,
+    height: 22,
+    borderRadius: 9,
+    backgroundColor: Colors.white,
     alignItems: "center",
+    justifyContent: "center",
   },
 });
