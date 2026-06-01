ae4a8ab4b0cf4a5e3eecc9b3755380b91f6bb8b5
priya1202 <priyachelani122@gmail.com>
2026-06-01 08:15:21 +0530

    Resolving conflicts

--- apps/src/components/Profile.tsx
+++ apps/src/components/Profile.tsx
@@ -1,9 +1,5 @@
 // Write this code Priya
-<<<<<<< HEAD
 import color from "@/constants/Colors";
-=======
-
->>>>>>> main
 import { Image, StyleSheet, Text, View } from "react-native";
 
 export default function Profile() {
@@ -44,20 +40,13 @@ const styles = StyleSheet.create({
     width: 60,
     height: 60,
     borderRadius: 45,
-<<<<<<< HEAD
     backgroundColor: color.blue,
-=======
-    backgroundColor: "#5FA1CA",
-    borderWidth: 2,
-    borderColor: "#5FA1CA",
->>>>>>> main
   },
 
   userInfo: {
     marginLeft: 20,
     flex: 1,
   },
-<<<<<<< HEAD
     description: {
     color: "black",
     fontSize: 18,
@@ -73,6 +62,4 @@ const styles = StyleSheet.create({
     textTransform: 'uppercase',
     wordWrap: 'break-word'
   }
-=======
->>>>>>> main
 });
