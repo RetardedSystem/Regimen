21d103b445bce380c802a67a9b095f4d3ec0d31b
vanshanium <vansh.singh8272@gmail.com>
2026-06-11 01:27:09 +0530

    No Error

--- apps/src/components/kanban/KanbanColumn.tsx
+++ apps/src/components/kanban/KanbanColumn.tsx
@@ -24,12 +24,8 @@ type Prop = {
   tasks: Task[];
   reloadBoard: () => void;
 };
-tasks: Task[];
-reloadBoard: () => void;
-};
 
 export default function KanbanColumn(props: Prop) {
-  const { title, container, header } = columnConfig[props.title];
   const { title, container, header } = columnConfig[props.title];
 
   return (
@@ -54,15 +50,7 @@ export default function KanbanColumn(props: Prop) {
             task={task}
             onStatusChange={props.reloadBoard}
           />
-        {
-            props.tasks.map((task) => (
-              <TaskCard
-                key={task.id}
-                task={task}
-                onStatusChange={props.reloadBoard}
-              />
-            ))
-          }
+        ))}
       </ScrollView>
     </View>
   );
@@ -74,22 +62,18 @@ const borderRadius = 10;
 // Global Kanban Element Style, all Kanban Column inherits it.
 const container: ViewStyle = {
   flex: 1,
-  flex: 1,
   height: "30%",
-  width: "100%",
   width: "100%",
   borderRadius: borderRadius,
   backgroundColor: Colors.white,
   alignItems: "center",
   shadowOffset: {
     width: 4,
-    width: 4,
     height: 4,
   },
   shadowOpacity: 0.2,
   shadowRadius: 10,
   elevation: 2,
-  marginBottom: 10,
   marginBottom: 10,
 };
 
@@ -110,31 +94,24 @@ const styles = StyleSheet.create({
   todo_container: {
     ...container,
     minHeight: "45%",
-    minHeight: "45%",
     shadowColor: Colors.red,
   },
 
-
   backlog_container: {
     ...container,
-    minHeight: "20%", // Make it Dynamic based on the number of tasks
     minHeight: "20%", // Make it Dynamic based on the number of tasks
     backgroundColor: Colors.red,
     shadowColor: Colors.red,
   },
 
-
   in_progress_container: {
     ...container,
-    minHeight: "31%",
     minHeight: "31%",
     shadowColor: Colors.yellow,
   },
 
-
   done_container: {
     ...container,
-    minHeight: "30%",
     minHeight: "30%",
     shadowColor: Colors.blue,
   },
@@ -147,18 +124,15 @@ const styles = StyleSheet.create({
     backgroundColor: Colors.red,
   },
 
-
   in_progress_header: {
     ...header,
     backgroundColor: Colors.yellow,
   },
 
-
   done_header: {
     ...header,
     backgroundColor: Colors.blue,
   },
-
 
   backlog_header: {
     ...header,
@@ -166,7 +140,6 @@ const styles = StyleSheet.create({
   },
 
   title: {
-    fontFamily: "Gabarito",
     fontFamily: "Gabarito",
     fontSize: 20,
     color: Colors.white,
@@ -188,32 +161,8 @@ const styles = StyleSheet.create({
     top: 44,
     width: "100%",
     height: "85%",
-    height: "85%",
   },
 });
-
-const columnConfig = {
-  todo: {
-    title: "Todo",
-    container: styles.todo_container,
-    header: styles.todo_header,
-  },
-  in_progress: {
-    title: "Progress",
-    container: styles.in_progress_container,
-    header: styles.in_progress_header,
-  },
-  done: {
-    title: "Done",
-    container: styles.done_container,
-    header: styles.done_header,
-  },
-  missed: {
-    title: "Backlog",
-    container: styles.backlog_container,
-    header: styles.backlog_header,
-  },
-};
 
 const columnConfig = {
   todo: {
